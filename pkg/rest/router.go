6aa553b5de3242fbdd874a80cc4bbfc1f53460e4
Paweł Gąsiorowski <PawelGasiorowski@ccs.nl>
2023-08-27 20:16:07 +0200

    Switched from Gin to plain http server

---
+++ pkg/rest/router.go
@@ -0,0 +1,55 @@
+package rest
+
+import (
+	"log"
+	"net/http"
+	"regexp"
+)
+
+type route struct {
+	method  string
+	path    *regexp.Regexp
+	handler func(w http.ResponseWriter, r *http.Request)
+}
+
+type handlerFunc func(w http.ResponseWriter, r *http.Request)
+
+type Router struct {
+	routes []*route
+}
+
+func (h *Router) GET(path string, handler handlerFunc) error {
+	return h.handleFunc("GET", path, handler)
+}
+
+func (h *Router) PUT(path string, handler handlerFunc) error {
+	return h.handleFunc("PUT", path, handler)
+}
+
+func (h *Router) POST(path string, handler handlerFunc) error {
+	return h.handleFunc("POST", path, handler)
+}
+
+func (h *Router) DELETE(path string, handler handlerFunc) error {
+	return h.handleFunc("DELETE", path, handler)
+}
+
+func (h *Router) handleFunc(method string, path string, handler handlerFunc) error {
+	pathRegExp, err := regexp.Compile(path)
+	if err != nil {
+		return err
+	}
+	h.routes = append(h.routes, &route{method: method, path: pathRegExp, handler: handler})
+	return nil
+}
+
+func (h *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for _, route := range h.routes {
+		if r.Method == route.method && route.path.MatchString(r.URL.Path) {
+			log.Printf("%v: %v", r.Method, r.URL.Path)
+			route.handler(w, r)
+			return
+		}
+	}
+	http.NotFound(w, r)
+}
